cc0213c6b97abe3f3755b64329cd4f1e989ef18c
mtagomori <mtagomori@mcfox.com.br>
2015-12-27 20:05:00 -0200

    Adds conta_deposito_id method

--- lib/myfinance/conta_deposito.rb
+++ lib/myfinance/conta_deposito.rb
@@ -3,5 +3,16 @@ module Myfinance
   def self.contas_deposito(entity_id)
     lget "/entities/#{entity_id}/deposit_accounts.json"
   end
-end
 
+  def self.conta_deposito_id(entity_id, nome)
+    mid = nil
+    contas_deposito(entity_id).each do | item |
+      conta = item["deposit_account"]
+      if conta["name"].strip == nome.strip
+        mid = conta["id"]
+        break
+      end
+    end
+    mid
+  end
+end
--- spec/features/conta_deposito_spec.rb
+++ spec/features/conta_deposito_spec.rb
@@ -7,4 +7,18 @@ describe 'Manipulando Contas de Depósito', type: :feature do
     expect(Myfinance).to receive(:lget).once.with("/entities/1/deposit_accounts.json").and_return contas_deposito_double
     expect(Myfinance.contas_deposito(1)).to eql(contas_deposito_double)
   end
+
+  describe '#conta_deposito_id' do
+    let!(:deposit_accounts_double) { [{"deposit_account"=>{"id"=>14330, "name"=>"Bradesco"}}, {"deposit_account"=>{"id"=>14331, "name"=>"Itaú"}}] }
+    before do
+      allow(Myfinance).to receive(:contas_deposito).with(1).and_return(deposit_accounts_double)
+    end
+
+    it "returns nil if no deposit account found" do
+      expect(Myfinance.conta_deposito_id(1, "Citi")).to be nil
+    end
+    it "returns informed deposit account" do
+      expect(Myfinance.conta_deposito_id(1, "Itaú")).to eql 14331
+    end
+  end
 end
