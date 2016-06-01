31ce0cef0e1cec4528eb918c319277a9980664b6
mtagomori <mtagomori@mcfox.com.br>
2016-06-01 16:56:08 -0300

    New Myfinance search interface for people

--- lib/myfinance/pessoa.rb
+++ lib/myfinance/pessoa.rb
@@ -1,36 +1,19 @@
 module Myfinance
 
   def self.pessoa_id(cnpj_ou_nome)
-    @everyone ||= all_people
-    mid = nil
-    @everyone.each do | pessoa |
-      if @account_id.to_i == pessoa['account_id']
-        if pessoa['federation_subscription_number_only_numbers'] == cnpj_ou_nome or pessoa['federation_subscription_number'] == cnpj_ou_nome or pessoa['name'].casecmp(cnpj_ou_nome) == 0
-          mid = pessoa['id']
-          break
-        end
-      end
-    end
-    mid
-  end
-
-  def self.all_people
-    everyone = []
-    response = lget "/people.json"
-    total_pages = get_last_page(response)
-    for page in 1..total_pages
-      response = lget "/people.json?page=#{page}"
-      response.each do | people |
-        everyone << people['person']
-      end
-    end
-    everyone
+    person = pessoa(cnpj_ou_nome)
+    person.nil? ? nil : person["id"]
   end
 
   def self.pessoa(cnpj_ou_nome)
-    mid = pessoa_id(cnpj_ou_nome)
-    response = lget "/people/#{mid}.json"
-    response['person'] if response
+    person = pesquisa_pessoa("federation_subscription_number", cnpj_ou_nome)
+    person ||= pesquisa_pessoa("name", cnpj_ou_nome)
+    person
+  end
+
+  def self.pesquisa_pessoa(campo, valor)
+    people =  Myfinance.lget URI.encode("/people.json?search[#{campo}]=#{valor}")
+    people.empty? ? nil : people[0]["person"]
   end
 
   def self.cria_pessoa( pessoa )
@@ -46,18 +29,5 @@ module Myfinance
     @everyone = nil
     lput "/people/#{people_id}.json", pessoa
   end
-
-
-  def self.get_last_page(response)
-    total_pages = 1
-    links = (response.header['link'].split(',') rescue [])
-    link = links.find{ |l| l.include?('last') }
-    if link
-      total_pages = (link.split(";")[0].split('?')[1].gsub('page=','').to_i rescue 1)
-    end
-    total_pages
-  end
-
-
 end
 
