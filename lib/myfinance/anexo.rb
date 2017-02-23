793a6a4d6f73bc017738438abd1479ea93574600
joselopesneto <jose.lopes.santos.neto@gmail.com>
2017-02-23 16:49:48 -0300

    forcando o encoding para utf 8

--- lib/myfinance/anexo.rb
+++ lib/myfinance/anexo.rb
@@ -4,18 +4,18 @@ module Myfinance
 
   def self.cria_e_anexa_arquivo(entity_id, receivable_id, nome_do_arquivo, conteudo_do_arquivo)
     file = nome_do_arquivo.split('.')
+    #:encoding => 'ascii-8bit' -- na verdade não altere o arquivo original
     attached_file = Tempfile.new([file[0], ".#{file[1]}"])
-    attached_file << conteudo_do_arquivo
+    attached_file << conteudo_do_arquivo.encode('UTF-8', :invalid => :replace, :undef => :replace )
     attached_file.rewind
     response = anexa_arquivo(entity_id, receivable_id, nome_do_arquivo, attached_file)
-    attached_file.close
-    attached_file.unlink
+    # attached_file.close
+    # attached_file.unlink
     response
   end
 
 
   def self.anexa_arquivo(entity_id, receivable_id, titulo, arquivo)
-    arquivo.rewind rescue nil
     post_data = { attachment: {
         attachment: arquivo,
         title: titulo,
@@ -23,6 +23,9 @@ module Myfinance
       }
     }
     response = multi_party_post( "/entities/#{entity_id}/attachments",post_data)
+    if response['attachment'][0] == "não pode ser vazio."
+      raise "O arquivo anexado foi vazio: #{arquivo}"
+    end
     response
   end
 
