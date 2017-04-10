00969cc67f800fe43fa495caa534ccbb634cee45
joselopesneto <jose.lopes.santos.neto@gmail.com>
2017-04-10 11:23:19 -0300

    Tratando eventuais erros na chamada de accounts

--- lib/myfinance/account.rb
+++ lib/myfinance/account.rb
@@ -1,9 +1,9 @@
 module Myfinance
 
-
   def self.account_id(nome)
     mid = nil
-    accounts.each do | item |
+    available_accounts = accounts
+    available_accounts.each do | item |
       item = item['account']
       if item['name'] == nome
         mid = item['id']
@@ -11,6 +11,8 @@ module Myfinance
       end
     end
     mid
+  rescue => e
+    raise "Não foi retornado um array de contas como esperado: #{available_accounts.inspect}. #{e.message}"
   end
 
   def self.accounts
--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -3,47 +3,47 @@ describe 'Account', type: :feature do
   require 'myfinance'
 
   it '#accounts' do
-    double accounts_double = double('account')
+    # double accounts_double = double('account')
     expect(Myfinance).to receive(:lget).once.with('/accounts.json').and_return accounts_double
     expect(Myfinance.accounts).to eql(accounts_double)
   end
 
-  describe "specific account" do
-    let(:response_double) { double("response", code: 200) }
-    describe "informed" do
+  describe 'specific account' do
+    let(:response_double) { double('response', code: 200) }
+    describe 'informed' do
       before do
         expect(Myfinance).to receive(:accounts).and_return response_double
-        Myfinance.setup "123456", false, 25
+        Myfinance.setup '123456', false, 25
       end
-      it "get" do
-        expect(Myfinance).to receive(:get).with("/entities.json", {:basic_auth=>{:username=>"123456", :password=>"x"}, :headers=>{"Content-Type"=>"application/json", "ACCOUNT_ID"=>"25"}})
+      it 'get' do
+        expect(Myfinance).to receive(:get).with('/entities.json', {:basic_auth=>{:username=> '123456', :password=> 'x'}, :headers=>{'Content-Type' => 'application/json', 'ACCOUNT_ID' => '25'}})
         Myfinance.entidades
       end
-      it "post" do
-        expect(Myfinance).to receive(:post).with("/people.json", {:basic_auth=>{:username=>"123456", :password=>"x"}, :body=>"{\"person\":{}}", :headers=>{"Content-Type"=>"application/json", "ACCOUNT_ID"=>"25"}})
+      it 'post' do
+        expect(Myfinance).to receive(:post).with('/people.json', {:basic_auth=>{:username=> '123456', :password=> 'x'}, :body=>"{\"person\":{}}", :headers=>{'Content-Type' => 'application/json', 'ACCOUNT_ID' => '25'}})
         Myfinance.cria_pessoa({})
       end
-      it "put" do
-        expect(Myfinance).to receive(:put).with("/people/30.json", {:basic_auth=>{:username=>"123456", :password=>"x"}, :body=>"{}", :headers=>{"Content-Type"=>"application/json", "ACCOUNT_ID"=>"25"}})
+      it 'put' do
+        expect(Myfinance).to receive(:put).with('/people/30.json', {:basic_auth=>{:username=> '123456', :password=> 'x'}, :body=> '{}', :headers=>{'Content-Type' => 'application/json', 'ACCOUNT_ID' => '25'}})
         Myfinance.atualiza_pessoa(30, {})
       end
     end
-    describe "not informed" do
+    describe 'not informed' do
       before do
         expect(Myfinance).to receive(:accounts).and_return response_double
         expect(Myfinance).to receive(:get_account_id).and_return nil
-        Myfinance.setup "123456", false
+        Myfinance.setup '123456', false
       end
-      it "get" do
-        expect(Myfinance).to receive(:get).with("/entities.json", {:basic_auth=>{:username=>"123456", :password=>"x"}, :headers=>{"Content-Type"=>"application/json"}})
+      it 'get' do
+        expect(Myfinance).to receive(:get).with('/entities.json', {:basic_auth=>{:username=> '123456', :password=> 'x'}, :headers=>{'Content-Type' => 'application/json'}})
         Myfinance.entidades
       end
-      it "post" do
-        expect(Myfinance).to receive(:post).with("/people.json", {:basic_auth=>{:username=>"123456", :password=>"x"}, :body=>"{\"person\":{}}", :headers=>{"Content-Type"=>"application/json"}})
+      it 'post' do
+        expect(Myfinance).to receive(:post).with('/people.json', {:basic_auth=>{:username=> '123456', :password=> 'x'}, :body=>"{\"person\":{}}", :headers=>{'Content-Type' => 'application/json'}})
         Myfinance.cria_pessoa({})
       end
-      it "put" do
-        expect(Myfinance).to receive(:put).with("/people/30.json", {:basic_auth=>{:username=>"123456", :password=>"x"}, :body=>"{}", :headers=>{"Content-Type"=>"application/json"}})
+      it 'put' do
+        expect(Myfinance).to receive(:put).with('/people/30.json', {:basic_auth=>{:username=> '123456', :password=> 'x'}, :body=> '{}', :headers=>{'Content-Type' => 'application/json'}})
         Myfinance.atualiza_pessoa(30, {})
       end
     end
